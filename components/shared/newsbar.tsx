1e495af9b710e287bad7ca5121023597b7568295
layerl <layerlain@yandex.ru>
2025-08-22 07:56:01 +0900

    dropdown for news

--- components/shared/newsbar.tsx
+++ components/shared/newsbar.tsx
@@ -1,46 +1,52 @@
+"use client";
+
 import React from "react";
 import { cn } from "@/lib/utils";
 import { Title } from "./title";
-import { ChevronDown } from "lucide-react";
+import { ChevronDown, ChevronUp } from "lucide-react";
+import { news } from "../../lib/constants";
 
 interface Props {
   className?: string;
 }
 
 export const Newsbar: React.FC<Props> = ({ className }) => {
+  const [visibleCount, setVisibleCount] = React.useState(4);
+
+  const showMore = () => {
+    setVisibleCount((prev) => prev + 2);
+  };
+
+  const showLess = () => {
+    setVisibleCount(4);
+  };
+
+  const hasMore = visibleCount < news.length;
+
+  const isExpandedToMax = visibleCount == news.length + 1;
+
   return (
     <div className={cn("py-4 px-4 rounded-sm mt-5 bg-white h-fit", className)}>
       <Title text="Интересное" size="xs" className="font-bold mb-2 cursor-pointer" />
-      <div className="mb-4 cursor-pointer hover:text-gray-500">
-        <p className="text-sm leading-4">
-          Объем выданной якутянам за полугодие ипотеки снизился вдвое{" "}
-        </p>
-        <p className="text-[11px] mt-1  text-gray-400">14 августа 2025</p>
-      </div>
-      <div className="mb-4 cursor-pointer hover:text-gray-500">
-        <p className="text-sm leading-4">
-          Якутские кинематографисты могут выиграть грант в 5 миллионов рублей на производство своего
-          короткометражного фильма
-        </p>
-        <p className="text-[11px] mt-1 text-gray-400">14 августа 2025</p>
-      </div>
-      <div className="mb-4 cursor-pointer hover:text-gray-500">
-        <p className="text-sm leading-4">
-          Nothing beats a Jet2 holiday: в парке Якутска появился новый аттракцион
-        </p>
-        <p className="text-[11px] mt-1 text-gray-400">13 августа 2025</p>
-      </div>
-      <div className="mb-2 cursor-pointer hover:text-gray-500">
-        <p className="text-sm leading-4">
-          Люди все такие же клевые!» Артем Cut The Crap – о прошедшем гик-фестивале, Якутии, местной
-          еде и творческих планах
-        </p>
-        <p className="text-[11px] mt-1  text-gray-400">12 августа 2025</p>
-      </div>
-      <div className="flex justify-end items-center cursor-pointer">
-        <ChevronDown size={15} />
-        <p className="text-[13px] font-bold ml-1">Показать еще</p>
-      </div>
+      {news.slice(0, visibleCount).map((newsItem) => (
+        <div key={newsItem.id} className="mb-4 cursor-pointer hover:text-gray-500">
+          <p className="text-sm leading-4">{newsItem.title}</p>
+          <p className="text-[11px] mt-1  text-gray-400">{newsItem.date}</p>
+        </div>
+      ))}
+      {hasMore && (
+        <div className="flex justify-end items-center cursor-pointer" onClick={showMore}>
+          <ChevronDown size={15} />
+          <p className="text-[13px] font-bold ml-1">Показать еще</p>
+        </div>
+      )}
+
+      {isExpandedToMax && (
+        <div className="flex justify-end items-center cursor-pointer" onClick={showLess}>
+          <ChevronUp size={15} />
+          <p className="text-[13px] font-bold ml-1">Скрыть</p>
+        </div>
+      )}
     </div>
   );
 };
