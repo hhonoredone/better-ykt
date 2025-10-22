079df80b97d43c8208ad5cbd9ddfb8c1d7227981
layerl <layerlain@yandex.ru>
2025-10-22 14:27:01 +0900

    fix

--- components/shared/service-card-wrapper.tsx
+++ components/shared/service-card-wrapper.tsx
@@ -27,30 +27,28 @@ export const ServiceCardWrapper: React.FC<Props> = ({
   link,
 }) => {
   return (
-    <Suspense fallback={<MaincardSkeleton />}>
-      <div
-        className={cn("rounded-md bg-white mt-5 overflow-hidden scroll-mt-22", className)}
-        ref={ref}
-        id={id}>
-        <div className={cn("h-10 rounded-t-md", serviceColor)} />
-        <div className="px-5 pb-5 mt-[-20px]">
-          <div className="flex items-start">
-            <Image
-              src={serviceImageUrl}
-              width={50}
-              height={50}
-              alt="doska"
-              className="border-2 border-white rounded-full bg-white"
-            />
-          </div>
-          <div className="flex justify-between items-center">
-            <Link href={link} target="_blank">
-              <Title text={title} className="font-bold mt-1 cursor-pointer" size="md" />
-            </Link>
-          </div>
-          {children}
+    <div
+      className={cn("rounded-md bg-white mt-5 overflow-hidden scroll-mt-22", className)}
+      ref={ref}
+      id={id}>
+      <div className={cn("h-10 rounded-t-md", serviceColor)} />
+      <div className="px-5 pb-5 mt-[-20px]">
+        <div className="flex items-start">
+          <Image
+            src={serviceImageUrl}
+            width={50}
+            height={50}
+            alt="doska"
+            className="border-2 border-white rounded-full bg-white"
+          />
         </div>
+        <div className="flex justify-between items-center">
+          <Link href={link} target="_blank">
+            <Title text={title} className="font-bold mt-1 cursor-pointer" size="md" />
+          </Link>
+        </div>
+        {children}
       </div>
-    </Suspense>
+    </div>
   );
 };
